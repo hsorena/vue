83dd6b86dcf73b9e9487b4d57893db4df8a1847a
sorena <ha.amini74@gmail.com>
2021-06-25 16:27:58 +0430

    clean document

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-Vue.component('task' , {
-    template: '<li><slot></slot></li>'
-});
 
-Vue.component('task-list' , {
-    template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
-    data() {
-        return {
-            tasks:[
-                { task:'Go to the store' , completed : true},
-                { task:'Go to the email' , completed : false},
-                { task:'Go to the farm' , completed : false},
-                { task:'Go to work' , completed : false}
-            ]
-        };
-    }
-})
 
 new Vue({
     el:'#root',
 
-    data(){
-        return{
-
-        }
-    }
 });
